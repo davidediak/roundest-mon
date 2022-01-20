40ff9fd0e572455dd20b61d6b7432602a0a604b9
Davide <davidediak@gmail.com>
2022-01-20 08:05:02 +0100

    basic UI in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import type {NextPage} from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
 
 const Home: NextPage = () => {
-    return <div className='="text-2xl text-red-200'>Hello World</div>;
+    return (
+        <div className="h-screen w-screen flex flex-col justify-center items-center">
+            <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
+            <div className="p-2"></div>
+            <div className="border rounded p-8 flex justify-between">
+                <div className="w-16 h-16 bg-red-200"></div>
+                <div className="p-8">VS</div>
+                <div className="w-16 h-16 bg-red-200"></div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
