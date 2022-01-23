06d32a9e612854f6f2b1b0249736b32e8a7e6df5
Davide <davidediak@gmail.com>
2022-01-23 11:57:44 +0100

    Added random pokemon id fecther

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,18 @@
+import { getOptionsForVote } from '@/utils/getRandomPokemon';
 import {trpc} from '@/utils/trpc';
 import type {NextPage} from 'next';
 
 const Home: NextPage = () => {
-    const {data, isLoading} = trpc.useQuery(['hello']);
+    const [first, second] = getOptionsForVote();
 
-    if (isLoading) return <div>Loading</div>
-
-    if (data) return <div>{data.greeting}</div>
     return (
         <div className="h-screen w-screen flex flex-col justify-center items-center">
             <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
             <div className="p-2"></div>
             <div className="border rounded p-8 flex justify-between">
-                <div className="w-16 h-16 bg-red-200"></div>
+                <div className="w-16 h-16 ">{first}</div>
                 <div className="p-8">VS</div>
-                <div className="w-16 h-16 bg-red-200"></div>
+                <div className="w-16 h-16 ">{second}</div>
             </div>
         </div>
     );
