3909353d9eb6f8c43fad2a4e74d3c1d2354d604d
Davide <davidediak@gmail.com>
2022-01-23 11:57:44 +0100

    BE pokemon data, disabled SSR temporary

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,19 +1,18 @@
 import * as trpc from '@trpc/server';
-import { z } from 'zod';
+import {PokemonClient} from 'pokenode-ts';
+import {z} from 'zod';
 
-export const appRouter = trpc.router().query('hello', {
-  input: z
-    .object({
-      text: z.string().nullish(),
-    })
-    .nullish(),
-  resolve({ input }) {
-    return {
-      greeting: `hello ${input?.text ?? 'world'}`,
-    };
-  },
+export const appRouter = trpc.router().query('get-pokemon-by-id', {
+    input: z.object({
+        id: z.number(),
+    }),
+    resolve({input}) {
+        const api = new PokemonClient();
+
+        const pokemon = api.getPokemonById(input.id);
+        return pokemon;
+    },
 });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
-
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,36 @@
-import { getOptionsForVote } from '@/utils/getRandomPokemon';
+import {getOptionsForVote} from '@/utils/getRandomPokemon';
 import {trpc} from '@/utils/trpc';
 import type {NextPage} from 'next';
+import {useMemo, useState} from 'react';
 
 const Home: NextPage = () => {
-    const [first, second] = getOptionsForVote();
+    const [[firstId, secondId], updateIds] = useState(() => getOptionsForVote());
+    const firstPokemon = trpc.useQuery(['get-pokemon-by-id', {id: firstId}]);
+    const secondPokemon = trpc.useQuery(['get-pokemon-by-id', {id: secondId}]);
+
+    if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
 
     return (
         <div className="h-screen w-screen flex flex-col justify-center items-center">
             <div className="text-2xl text-center">Which Pokemon is Rounder?</div>
             <div className="p-2"></div>
             <div className="border rounded p-8 flex justify-between">
-                <div className="w-16 h-16 ">{first}</div>
+                <div className="w-64 h-64 flex flex-col">
+                    <img
+                        src={firstPokemon.data?.sprites.front_default ?? undefined}
+                        className="w-full"
+                    />
+                    <div className="text-xl text-center capitalize mt-[-2rem]">{firstPokemon.data?.name}</div>
+                </div>
                 <div className="p-8">VS</div>
-                <div className="w-16 h-16 ">{second}</div>
+                <div className="w-64 h-64 flex flex-col">
+                    <img
+                        src={secondPokemon.data?.sprites.front_default ?? undefined}
+                        className="w-full"
+                    />
+                    <div className="text-xl text-center capitalize mt-[-2rem]">{secondPokemon.data?.name}</div>
+                </div>
+                <div className="p-2"></div>
             </div>
         </div>
     );
